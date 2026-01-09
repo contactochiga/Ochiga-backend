fb9471a3abfe6e23b26977344dcb9f22b61619ed
contactochiga <contactochiga@gmail.com>
2026-01-09 09:28:15 +0100

    Update security.policy.ts

--- src/core/control-plane/policies/security.policy.ts
+++ src/core/control-plane/policies/security.policy.ts
@@ -1,5 +1,7 @@
-import { Signal } from "../signal.types";
-import { Intent } from "../intent.types";
+// src/core/control-plane/policies/security.policy.ts
+
+import { Signal } from "../contracts/signal.types";
+import { Intent } from "../contracts/intent.types";
 
 export function securityPolicy(_: Signal): Intent[] {
   return [];
