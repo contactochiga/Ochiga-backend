df240642a24241819996772ab6fcebcfdf9423fa
contactochiga <contactochiga@gmail.com>
2026-01-11 13:57:13 +0100

    Update residents.ts

--- src/routes/residents.ts
+++ src/routes/residents.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { supabaseAdmin } from "../supabase/client";
+import { supabaseAdmin } from "../supabase/supabaseClient";
 import { requireAuth } from "../middleware/auth";
 import { requireRole } from "../middleware/roles";
 import crypto from "crypto";
