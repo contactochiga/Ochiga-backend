3fc8e1293812d2ceda839cfeeecc0c7184772173
contactochiga <contactochiga@gmail.com>
2026-02-17 14:55:20 +0100

    Update paystack.ts

--- src/utils/paystack.ts
+++ src/utils/paystack.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PAYSTACK_SECRET = process.env.PAYSTACK_SECRET_KEY!;
+const PAYSTACK_SECRET = process.env.PAYSTACK_SECRET_KEY;
 
 export const paystack = axios.create({
   baseURL: "https://api.paystack.co",
